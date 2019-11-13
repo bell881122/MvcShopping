052b4ec1e3eb4a7a915e7da4c99f876e971cc72d
sandy <sandy@atinfo.com.tw>
2019-11-13 16:32:22 +0800

    上傳專案

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcShopping.Models
+{
+    [DisplayName("商品類別")]
+    [DisplayColumn("Name")]
+    public class ProductCategory
+    {
+        [Key]
+        public int Id{ get; set; }
+
+        [DisplayName("商品類別名稱")]
+        [Required(ErrorMessage="請輸入商品類別名稱")]
+        [Maxlength(20,ErrorMessage="類別名稱不可超過20個字")]
+        public string Name{ get; set; }
+
+    }
+}
