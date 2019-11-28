d7e76e50fffa0c406498683c6d9661f2e6b5d1c5
sandy <sandy@atinfo.com.tw>
2019-11-28 23:18:34 +0800

    增加 Login 視圖

---
+++ ViewModel/MenberLoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcShopping.ViewModel
+{
+    public class MenberLoginViewModel
+    {
+        [DisplayName("會員帳號")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "請輸入Eamil地址")]
+        [Required(ErrorMessage ="請輸入{0}")]
+        public string email { get; set; }
+
+        [DisplayName("會員密碼")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "請輸入{0}")]
+        public string password { get; set; }
+    }
+}
